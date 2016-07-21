dd28d6f0db49a8f0904f4eab80934e3500b310fe
Alex Ellis <alexellis2@gmail.com>
2016-07-21 19:31:21 +0100

    Tidy up Program.cs

--- src/RateCalc.App/Program.cs
+++ src/RateCalc.App/Program.cs
@@ -9,24 +9,33 @@ namespace RateCalc.App
 {
     public class Program
     {
+        private class LenderActivity {
+            public LoanOffer GetLoanOffer(string fileName, double target) {
+                CsvParser parser = new CsvParser(new DiskFileSource());
+                IEnumerable<CsvLine> lines = parser.Read(fileName);
+                var lenderRepo = new LenderRepository(lines);
+                LenderTable table = lenderRepo.Read();
+                var loan = new Loan(table);
+
+                return loan.Request(target);
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine(File.ReadAllText("market.csv"));
+            var activity = new LenderActivity();
 
-            CsvParser parser = new CsvParser(new DiskFileSource());
-            IEnumerable<CsvLine> lines = parser.Read("market.csv");
-            var lenderRepo = new LenderRepository(lines);
-            LenderTable table = lenderRepo.Read();
-            var loan = new Loan(table);
-            
+            // TODO: Parse from command-line.
             double target = 1000;
-            var offer = loan.Request(target);
+            var offer = activity.GetLoanOffer("market.csv", target);
 
             var projector = new LoanProjector();
             var projection = projector.Get(offer.Rate, 36, offer.Target);
-            Console.WriteLine("Rate: " + offer.Rate.ToString() +" target=" + offer.Target);
-            Console.WriteLine("Payment: " + projection.Payment.ToString());
-            Console.WriteLine("Total: " + projection.TotalPayable.ToString());
+
+            // Could move formatting into class for sake of SRP / testability.
+            Console.WriteLine("Rate: " + String.Format("{0:0.0}",(offer.Rate*100)) +" target=" + offer.Target);
+            Console.WriteLine("Payment: " + String.Format("{0:0.00}", projection.Payment));
+            Console.WriteLine("Total: " + String.Format("{0:0.00}", projection.TotalPayable));
         }
     }
 }
